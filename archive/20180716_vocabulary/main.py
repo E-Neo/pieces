45114b9012bfe39dcc17c55baa68a297c2a4623f
E-Neo <e-neo@qq.com>
2018-08-01 11:21:08 +0800

    start reading microeconomics

--- archive/20180716_vocabulary/main.py
+++ archive/20180716_vocabulary/main.py
@@ -14,12 +14,20 @@ def create_table(cursor):
                    '(word VARCHAR(64) PRIMARY KEY, '
                    'chinese VARCHAR(1024), '
                    'description VARCHAR(1024))')
+    cursor.execute('CREATE TABLE IF NOT EXISTS word_strength '
+                   '(word VARCHAR(64) PRIMARY KEY, '
+                   'strength INT)')
 
 
 def insert(cursor, word, status):
     commit_time = datetime.now().astimezone().isoformat()
     cursor.execute('INSERT INTO recite_info VALUES (?,?,?)',
                    (word, commit_time, status))
+
+
+def update_word_strength(cursor, word, strength):
+    cursor.execute('UPDATE word_strength SET strength = ? WHERE word = ?',
+                   (strength, word))
 
 
 def commit_close(connection):
@@ -40,7 +48,12 @@ def recite(cursor):
         while True:
             word = input('word: ').strip()
             if not spell_correct(word):
-                break
+                print('Please retype that word to make sure it is not a typo!')
+                double_check = input('word: ').strip()
+                if double_check != word:
+                    print('Skipped!')
+                    print('--------------------')
+                    continue
             status = input('status: ').strip()
             print('--------------------')
             if not status_pattern.fullmatch(status):
@@ -48,11 +61,17 @@ def recite(cursor):
             insert(cursor, word, status)
             count += 1
     except KeyboardInterrupt:
-        return count
-    return count
+        print('total: {}'.format(count))
+    print('total: {}'.format(count))
 
 
 def _review(cursor, query):
+    try:
+        print('sync...', end='', flush=True)
+        sync(cursor)
+        print('done')
+    except Exception as e:
+        print(e)
     cursor.execute(query)
     status_pattern = re.compile(r'[01]')
     rows = cursor.fetchall()
@@ -76,10 +95,21 @@ def _review(cursor, query):
             status = input('status: ')
             if status_pattern.fullmatch(status):
                 insert(cursor, word, status)
+                strength = calculate_strength(cursor, word)
+                update_word_strength(cursor, word, strength)
                 count += 1
                 print('--------------------')
     except KeyboardInterrupt:
-        return count
+        print('total: {}'.format(count))
+    print('total: {}'.format(count))
+
+
+def _list(cursor, query):
+    count = 0
+    for row in cursor.execute(query):
+        word = row[0]
+        print(word)
+        count += 1
     return count
 
 
@@ -104,7 +134,24 @@ def look_up(word):
             r.content)
 
 
+def calculate_strength(cursor, word):
+    cursor.execute('SELECT status FROM recite_info '
+                   'WHERE word = ? ORDER BY commit_time',
+                   (word,))
+    rows = cursor.fetchall()
+    strength = 0
+    for row in rows:
+        status = row[0]
+        if status == 0:
+            if strength != 0:
+                strength -= 1
+        else:
+            strength += 1
+    return strength
+
+
 def sync(cursor):
+    # sync vocabulary and audio
     cursor.execute('SELECT DISTINCT word FROM recite_info WHERE word NOT IN '
                    '(SELECT word FROM vocabulary)')
     rows = cursor.fetchall()
@@ -116,9 +163,20 @@ def sync(cursor):
         with open('audio/{}.mp3'.format(word), 'wb') as f:
             f.write(audio)
 
+    # sync word_strength
+    cursor.execute('SELECT DISTINCT word FROM recite_info WHERE word NOT IN '
+                   '(SELECT word FROM word_strength)')
+    rows = cursor.fetchall()
+    for row in rows:
+        word = row[0]
+        strength = calculate_strength(cursor, word)
+        cursor.execute('INSERT INTO word_strength VALUES (?,?)',
+                       (word, strength))
+
 
 def main():
     options = {
+        'sync': sync,
         'recite': recite,
 
         'review_within_24hours': lambda cursor:
@@ -139,17 +197,48 @@ def main():
 
         'review_by_strength': lambda cursor:
         _review(cursor,
-                "SELECT word FROM "
-                "(SELECT word, min(commit_time) as initial_time, "
-                "sum(status) as strength "
-                "FROM recite_info GROUP BY word) "
-                "ORDER BY strength, initial_time LIMIT 50"),
+                "SELECT word FROM"
+                "(SELECT word FROM "
+                "(SELECT t1.word, t1.strength, t2.ct FROM word_strength t1 "
+                "JOIN "
+                "(SELECT word, min(commit_time) AS ct FROM recite_info "
+                "GROUP BY word) t2 ON t1.word = t2.word) "
+                "ORDER BY strength, ct LIMIT 50) "
+                "ORDER BY random()"),
 
         'review_old': lambda cursor:
         _review(cursor,
-                "SELECT word FROM "
+                "SELECT word FROM (SELECT word FROM "
                 "(SELECT word, max(commit_time) AS last_time "
-                "FROM recite_info GROUP BY word) ORDER BY last_time LIMIT 50")
+                "FROM recite_info GROUP BY word) ORDER BY last_time LIMIT 50)"
+                "ORDER BY random()"),
+
+        'list_all': lambda cursor:
+        _list(cursor,
+              "SELECT DISTINCT word from recite_info"),
+
+        'list_within_24hours': lambda cursor:
+        _list(cursor,
+              "SELECT word FROM "
+              "(SELECT word, min(commit_time) as initial_time "
+              "FROM recite_info GROUP BY word) "
+              "WHERE initial_time >= datetime('now', '-1 day') "),
+
+        'list_forgotten': lambda cursor:
+        _list(cursor,
+              "SELECT word FROM "
+              "(SELECT word, status FROM recite_info "
+              "WHERE (word, commit_time) "
+              "IN (SELECT word, max(commit_time) FROM recite_info "
+              "GROUP BY word)) WHERE status = 0"),
+
+        'list_by_strength': lambda cursor:
+        _list(cursor,
+              "SELECT word FROM "
+              "(SELECT t1.word, t1.strength, t2.ct FROM word_strength t1 "
+              "JOIN "
+              "(SELECT word, min(commit_time) AS ct FROM recite_info "
+              "GROUP BY word) t2 ON t1.word = t2.word) ORDER BY strength, ct")
     }
     if len(sys.argv) == 1:
         for k in options.keys():
@@ -158,13 +247,7 @@ def main():
     connection = sqlite3.connect('db.sqlite3')
     cursor = connection.cursor()
     create_table(cursor)
-    try:
-        print('sync...', end='', flush=True)
-        sync(cursor)
-        print('done')
-    except Exception as e:
-        print(e)
-    print('total: {}'.format(options[sys.argv[1]](cursor)))
+    options[sys.argv[1]](cursor)
     commit_close(connection)
 
 
